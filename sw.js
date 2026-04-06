4caa3d95d8e490ec56f11d551ffe5d71bd102d6d
seemsfunny21 <seemsfunny21@gmail.com>
2026-04-06 07:34:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Status: RE-ENGINEERED FOR GTMETRIX GRADE A+
    ========================================================================== */
 
-const CACHE_NAME = 'pegasus-shield-v2.13'; 
+const CACHE_NAME = 'pegasus-shield-v2.14'; 
 
 const ASSETS_TO_CACHE = [
     './', 
