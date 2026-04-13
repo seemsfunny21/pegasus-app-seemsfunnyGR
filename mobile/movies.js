038dc576de244d7a86f1eb9970f0377e53638e02
seemsfunny21 <seemsfunny21@gmail.com>
2026-04-13 19:11:39 +0300

    Update movies.js

--- mobile/movies.js
+++ mobile/movies.js
@@ -82,7 +82,6 @@
         viewDiv.innerHTML = `
             <button class="btn-back" onclick="openView('home')">◀ ΕΠΙΣΤΡΟΦΗ</button>
             <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 15px;">
-                <div class="section-title" style="margin: 0;">ΤΑΙΝΙΕΣ / ΣΕΙΡΕΣ</div>
                 <button id="btnAddMovie" class="primary-btn" style="width: auto; margin: 0; padding: 5px 10px; font-size: 10px; border-radius: 8px;" onclick="window.PegasusMovies.toggleAddForm()">
                     + ΝΕΑ ΤΑΙΝΙΑ
                 </button>
