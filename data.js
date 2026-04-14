6df824c573e221aaea5fae483605fba0e1769723
seemsfunny21 <seemsfunny21@gmail.com>
2026-04-14 09:07:07 +0300

    Update data.js

--- data.js
+++ data.js
@@ -6,6 +6,9 @@
 
 // 1. DEFAULT PLAN: IRON_LOGIC (5 Days Active / Mon & Thu Rest)
 window.program = {
+    "Δευτέρα": [ 
+        { name: "Stretching", sets: 1, muscleGroup: "None", weight: "0" }
+    ],
     "Τρίτη": [ 
         { name: "Chest Press", sets: 6, muscleGroup: "Στήθος", weight: "54" },
         { name: "Chest Flys", sets: 5, muscleGroup: "Στήθος", weight: "42" },
@@ -20,6 +23,9 @@ window.program = {
         { name: "Preacher Curls", sets: 5, muscleGroup: "Χέρια", weight: "30" },
         { name: "Plank", sets: 5, muscleGroup: "Κορμός", weight: "0" }
     ],
+    "Πέμπτη": [ 
+        { name: "Stretching", sets: 1, muscleGroup: "None", weight: "0" }
+    ],
     "Παρασκευή": [ 
         { name: "Medium Pulldowns", sets: 6, muscleGroup: "Πλάτη", weight: "36" },
         { name: "Bent Over Rows", sets: 6, muscleGroup: "Πλάτη", weight: "30" },
@@ -30,7 +36,7 @@ window.program = {
     "Σάββατο": [ 
         { name: "Leg Extensions", sets: 8, muscleGroup: "Πόδια", weight: "36" },
         { name: "Sit Ups", sets: 6, muscleGroup: "Κορμός", weight: "0" },
-        { name: "Ab Crunches", sets: 6, muscleGroup: "Κορμός", weight: "30" },
+        { name: "Ab Crunches", sets: 6, muscleGroup: "Κορμός", weight: "30" }, // 🎯 Διορθώθηκε: Τώρα έχει 30kg και το Σάββατο
         { name: "Plank", sets: 5, muscleGroup: "Κορμός", weight: "0" }
     ],
     "Κυριακή": [ 
