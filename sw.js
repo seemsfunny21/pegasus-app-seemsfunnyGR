f579ee47198e259df74b8d36ccaeb14b2e92a044
seemsfunny21 <seemsfunny21@gmail.com>
2026-04-24 07:37:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Status: FINAL STABLE | HARDENED: SAME-ORIGIN ONLY + GET ONLY + SAFE CACHE PUT
    ========================================================================== */
 
-const CACHE_NAME = 'pegasus-shield-v3.44-FINAL-104';
+const CACHE_NAME = 'pegasus-shield-v3.45-FINAL-105';
 
 const ASSETS_TO_CACHE = [
     '.',
